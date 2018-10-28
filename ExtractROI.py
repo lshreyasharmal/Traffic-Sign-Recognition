aa532940aa022a140697913b5cfb3e9e830d889f
Shreya Sharma <shreya15096@iiitd.ac.in>
2018-10-29 04:51:09 +0530

    Add files via upload

--- ExtractROI.py
+++ ExtractROI.py
@@ -4,8 +4,19 @@ from PIL import Image, ImageEnhance
 import sys
 import math
 from matplotlib import pyplot as plt
-path = r"C:\Users\MyPC\Desktop\Images\CroppedImages"
-img = cv2.imread(r'C:\Users\MyPC\Desktop\Images\speed_limit_80.jpg')
+title = "\mini_roundabout"
+directory = r"C:\Users\MyPC\Desktop\Images\CroppedImages"
+path = directory+title
+import os
+if not os.path.exists(path):
+    os.makedirs(path)
+if not os.path.exists(path+"\Circle"):
+    os.makedirs(path+"\Circle")
+if not os.path.exists(path+"\Triangle"):
+    os.makedirs(path+"\Triangle")
+if not os.path.exists(path+"\Octagon"):
+    os.makedirs(path+"\Octagon")
+img = cv2.imread(r'C:\Users\MyPC\Desktop\Images'+title+'.jpg')
 bgr=img
 #convert image from rgb to lab - l:lightness, a:green-red and b-blue yellow, numerical values
 lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
@@ -80,21 +91,109 @@ for i, cnt in enumerate (contours):
     approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
 
     if len(approx) > 10:
+        #circle sign
         x, y, w, h = cv2.boundingRect(cnt)
-        x = int(x-0.1*w)
-        y = int(y-0.1*h)
-        h = int(h+h*0.2)
-        w = int(w+0.2*w)
-        crop_img = img[y:y+h, x:x+w]
-        cv2.imwrite(path+r"\Crop_img_"+str(number)+".png",crop_img)
-        number+=1
+        wdth, ht, g = img.shape
+        
+        x1 = int(x-0.1*w)
+        y1 = int(y-0.1*h)
+        h1 = int(h+h*0.2)
+        w1 = int(w+0.2*w)
+        if(x1>=0):
+            x = x1
+        if(y1>=0):
+            y = y1
+        if(w<=wdth):
+            w = w1
+        if(h<=ht):
+            h = h1
         
         try:
             crop_img = img[y:y+h, x:x+w]
-            mask = cv2.inRange(crop_img, (17, 15, 100), (50, 56, 200))
+            img_hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+            mask1 = cv2.inRange(img_hsv, (0,50,20), (5,255,255))
+            mask2 = cv2.inRange(img_hsv, (175,50,20), (180,255,255))
+            mask = cv2.bitwise_or(mask1, mask2 )
             output = cv2.bitwise_and(crop_img,crop_img,mask=mask)
             if(not np.all(output==0) and np.count_nonzero(output)>0.1*output.shape[0]*output.shape[1]):
-                cv2.imwrite(path+r"\Crop_img_"+str(number)+".png",crop_img)
+                cv2.imwrite(path+r"\Circle\Crop_img_"+str(number)+".png",crop_img)
+                number+=1
+            
+            mask = cv2.inRange(img_hsv, (90,0,0), (140,255,255))
+            output = cv2.bitwise_and(crop_img,crop_img,mask=mask)
+
+            if(not np.all(output==0) and np.count_nonzero(output)>0.1*output.shape[0]*output.shape[1]):
+                cv2.imwrite(path+r"\Circle\Crop_img_"+str(number)+".png",crop_img)
+                number+=1
+        except:
+            print("error")
+    if len(approx)== 3:
+        #triangle sign
+        x, y, w, h = cv2.boundingRect(cnt)
+        wdth, ht, g = img.shape
+        
+        x1 = int(x-0.1*w)
+        y1 = int(y-0.1*h)
+        h1 = int(h+h*0.2)
+        w1 = int(w+0.2*w)
+        if(x1>=0):
+            x = x1
+        if(y1>=0):
+            y = y1
+        if(w<=wdth):
+            w = w1
+        if(h<=ht):
+            h = h1
+            
+        try:
+            crop_img = img[y:y+h, x:x+w]
+            img_hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+            mask1 = cv2.inRange(img_hsv, (0,50,20), (5,255,255))
+            mask2 = cv2.inRange(img_hsv, (175,50,20), (180,255,255))
+            mask = cv2.bitwise_or(mask1, mask2 )
+            output = cv2.bitwise_and(crop_img,crop_img,mask=mask)
+
+            if(not np.all(output==0) and np.count_nonzero(output)>0.1*output.shape[0]*output.shape[1]):
+                cv2.imwrite(path+r"\Triangle\Crop_img_"+str(number)+".png",crop_img)
+                number+=1
+           
+            
+            mask = cv2.inRange(img_hsv, (90,0,0), (140,255,255))
+            output = cv2.bitwise_and(crop_img,crop_img,mask=mask)
+
+            if(not np.all(output==0) and np.count_nonzero(output)>0.5*output.shape[0]*output.shape[1]):
+                cv2.imwrite(path+r"\Triangle\Crop_img_"+str(number)+".png",crop_img)
+                number+=1
+        except:
+            print("error")
+    if len(approx)== 8:
+        #triangle sign
+        x, y, w, h = cv2.boundingRect(cnt)
+        wdth, ht, g = img.shape
+        
+        x1 = int(x-0.1*w)
+        y1 = int(y-0.1*h)
+        h1 = int(h+h*0.2)
+        w1 = int(w+0.2*w)
+        if(x1>=0):
+            x = x1
+        if(y1>=0):
+            y = y1
+        if(w<=wdth):
+            w = w1
+        if(h<=ht):
+            h = h1
+            
+        try:
+            crop_img = img[y:y+h, x:x+w]
+            img_hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+            mask1 = cv2.inRange(img_hsv, (0,50,20), (5,255,255))
+            mask2 = cv2.inRange(img_hsv, (175,50,20), (180,255,255))
+            mask = cv2.bitwise_or(mask1, mask2 )
+            output = cv2.bitwise_and(crop_img,crop_img,mask=mask)
+
+            if(not np.all(output==0) and np.count_nonzero(output)>0.1*output.shape[0]*output.shape[1]):
+                cv2.imwrite(path+r"\Octagon\Crop_img_"+str(number)+".png",crop_img)
                 number+=1
         except:
             print("error")
